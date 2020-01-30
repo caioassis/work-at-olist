39dbf2ccc8767a8705004c6e496d5509da77e9fd
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2020-01-30 18:41:05 -0300

    Created Author model

--- library/models.py
+++ library/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+        indexes = [
+            models.Index(fields=['name'], name='idx_author_name')
+        ]
