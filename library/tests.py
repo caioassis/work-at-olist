21e7ae33e01102bab43a82059951e2189dc8aa53
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2020-01-30 18:46:16 -0300

    Initial tests for author

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Author
 
-# Create your tests here.
+
+class AuthorTestCase(TestCase):
+
+    def test_create_new_author(self):
+        author = Author.objects.create(name='Dan Brown')
+        self.assertEqual(author.name, 'Dan Brown')
+        authors = Author.objects.all()
+        self.assertEqual(authors.count(), 1)  # Only 1 author is expected to be in database
