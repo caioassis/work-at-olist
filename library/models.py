99261c0c38b8cfe3832fce9c31f95929e18d5c7d
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2020-01-30 23:15:15 -0300

    Book Model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -14,4 +15,24 @@ class Author(models.Model):
         verbose_name_plural = 'Authors'
         indexes = [
             models.Index(fields=['name'], name='idx_author_name')
+        ]
+
+
+class Book(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=60)
+    edition = models.PositiveSmallIntegerField(verbose_name='Edition', validators=[MinValueValidator(1)], default=1)
+    publication_year = models.PositiveIntegerField(verbose_name='Publication Year')
+    authors = models.ManyToManyField(verbose_name='Authors', to=Author, related_name='books')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.name} (Edition {self.edition}) - {self.publication_year}'
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        indexes = [
+            models.Index(fields=['name'], name='idx_book_name'),
+            models.Index(fields=['publication_year'], name='idx_book_publication_year')
         ]
