89b1a642c7fc7377b69855c5be707b18bafa5679
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2020-01-31 19:49:17 -0300

    Added Pagination to Author and Book views

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from .filters import AuthorFilterSet, BookFilterSet
 from .models import Author, Book
@@ -8,12 +9,14 @@ from .serializers import AuthorSerializer, BookListSerializer, BookSerializer
 class AuthorViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    pagination_class = PageNumberPagination
     filterset_class = AuthorFilterSet
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     filterset_class = BookFilterSet
+    pagination_class = PageNumberPagination
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
--- library_project/settings.py
+++ library_project/settings.py
@@ -82,5 +82,6 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
     'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser'],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 10
 }
